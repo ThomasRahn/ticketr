60720f814faf7c342e0e5f1b87200fc1c9d39cb0
ThomasRahn <thomas@rahn.ca>
2018-03-15 09:37:00 -0400

    Added styling and some design

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -4,12 +4,31 @@ class TicketsController < ApplicationController
     @tickets = Ticket.all
   end
 
-
   def show
   	@ticket = Ticket.find(params[:id])
   end
 
+  def new
+  	@ticket = Ticket.new
+  end
+
+  def create
+  	@ticket = Ticket.new(post_params)
+
+  	if(@ticket.save)
+  		redirect_to @ticket
+  	else
+  		render 'new'
+  	end
+  end
+
   def complete
-  	Ticket.find(params[:id]).update(is_completed: true)
+  	Ticket.find(params[:id]).complete
+
+  	render 'index'
+  end
+
+  private def post_params
+  	params.require(:ticket).permit(:task, :description)
   end
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,3 +1,10 @@
 class Ticket < ApplicationRecord
   has_many :comments
+  validates :task, presence: true,
+  					length: {minimum: 5}
+
+
+  def complete
+  	is_completed = true;
+  end
 end
