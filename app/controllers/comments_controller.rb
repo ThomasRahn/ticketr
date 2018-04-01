86fd361d38a4a1b504e4392b669655b4301d8eca
ThomasRahn <thomas@rahn.ca>
2018-03-31 20:45:04 -0400

    added active_link_to gem for nav bar active flag on current url

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,10 +6,10 @@ class CommentsController < ApplicationController
   	comment = params[:comment]
 
   	@comment = Comment.new({ ticket_id: ticket, user_id: user, comment: comment})
-    @comment.user = User.find(session[:user_id])
+    @user = User.find(session[:user_id])
 
   	if(@comment.save)
-  		render json: @comment
+  		render json: { "comment" => @comment, "username" => @user.firstname, "date" => localize(@comment.created_at, :format => :long) }
   	else
   		render json: { "success" => false }
   	end
