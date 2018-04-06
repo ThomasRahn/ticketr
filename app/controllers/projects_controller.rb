108fa19773773efb1c3195bb6e58523328c82637
ThomasRahn <thomas@rahn.ca>
2018-04-05 20:07:14 -0400

    Added form for new project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,7 +3,31 @@ class ProjectsController < ApplicationController
     @projects = Project.all
   end
 
+  def new 
+  	@project = Project.new
+
+  	@users = User.all
+  end
+
+  def create
+  	@project = Project.new(post_params)
+
+  	@project.user_id = params[:user_id]
+  	
+  	if(@project.save)
+  		redirect_to @project
+  	else
+  		@users = User.all
+  		render 'new'
+  	end
+  end
+
   def show
   	@project = Project.includes(:tickets).find(params[:id])
   end
+
+	private def post_params
+		params.require(:project).permit(:name, :description, :user_id)
+	end
 end
+
